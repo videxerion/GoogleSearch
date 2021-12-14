5964020c9e44596b57abcd00c077b7418bd9df82
videxerion <videxytgamer@gmail.com>
2021-12-14 22:39:06 +0000

    added function for parse metadata from docx files

--- gs_modules/metadata.py
+++ gs_modules/metadata.py
@@ -1,7 +1,8 @@
 import os
+import zipfile
+import xmltodict
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
-
 
 class metadata_module():
     def get_meta_data(self):
@@ -9,7 +10,7 @@ class metadata_module():
         return_array = []
         for i in range(len(fileList)):
             file_name = fileList[i]
-            print(f'get metadata {file_name}...')
+            #print(f'get metadata {file_name}...')
             if file_name.endswith('pdf'):
                 pdf_file = open(f'search_files/{file_name}', 'rb')
                 pdf_data = PDFParser(pdf_file)
@@ -29,7 +30,18 @@ class metadata_module():
                         value_dict.update({keys_array[h]: value})
                     return_array.append([file_name, value_dict])
             elif file_name.endswith('docx'):
-                pass
+                dict_for_return = {}
+                docx_file = zipfile.ZipFile(f'search_files/{file_name}','r')
+                core_file_xml = docx_file.read('docProps/core.xml')
+                docx_file.close()
+                core_file_dic = xmltodict.parse(core_file_xml, encoding='utf-8')
+                keys_core_file_dict = list(core_file_dic['cp:coreProperties'].keys())
+                for g in range(len(keys_core_file_dict)):
+                    value = core_file_dic["cp:coreProperties"][keys_core_file_dict[g]]
+                    key = keys_core_file_dict[g]
+                    if value is not None and str(type(value)) != "<class 'collections.OrderedDict'>" and value[0] != 'h':
+                        dict_for_return.update({f'{key[key.find(":") + 1:]}': f'{value}'})
+                return_array.append([file_name, dict_for_return])
         return return_array
 
 
