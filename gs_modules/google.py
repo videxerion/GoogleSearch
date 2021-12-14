b85e6ba485dee6d262c4eb609271d3129c474678
videxerion <videxytgamer@gmail.com>
2021-12-14 22:38:41 +0000

    added check to ban in google

--- gs_modules/google.py
+++ gs_modules/google.py
@@ -3,36 +3,41 @@ import time
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
 
+
+class GoogleBan(Exception):
+    def __init__(self):
+        self.txt = 'Ban in google'
+
+
 class google_module():
     def __init__(self):
         self.session = requests.session()
         self.headres = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36',
             'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'ru,ru-MD;q=0.5',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36'
+            'Accept': '*/*', 'Connection': 'keep-alive'
         }
+        self.session.headers = self.headres
 
     def search(self, request):
         g = 0
         return_array = []
         while True:
             time.sleep(1)
-            html = self.session.get(f'https://www.google.com/search?q={request}&start={g}0')
+            html = self.session.get(f'https://www.google.com/search?q={request}&start={g}0', headers=self.headres)
+            if html.status_code == 429:
+                raise GoogleBan
             soup = BeautifulSoup(html.text, 'html.parser')
-            links_not_clean = soup.find_all('div', {'class': 'kCrYT'})
+            links_not_clean = soup.find_all('div', {'class': 'yuRUbf'})
             links_clean = []
             for i in range(len(links_not_clean)):
                 string = str(links_not_clean[i])
                 if string.find('href') != -1:
-                    links_clean.append(unquote(unquote(string[string.find('/url?q=') + 7: string.find('&amp')])))
-                    # print(string[string.find('/url?q=') + 7: string.find('&amp')])
+                    links_clean.append(unquote(unquote(string[string.find('" href="') + 8: string.find('" ping="')])))
             if links_clean == []:
                 return return_array
             else:
                 for i in range(len(links_clean)):
                     return_array.append(links_clean[i])
             g += 1
-
 #  Copyright (c) 2021 videxerion
