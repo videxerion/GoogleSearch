9b923faffd7a557c789756f14916f825b6198a32
videxerion <videxytgamer@gmail.com>
2021-12-15 20:20:59 +0000

    edit logo, added colors

--- gs_modules/logo.py
+++ gs_modules/logo.py
@@ -1,11 +1,12 @@
+from colorama import init
+from colorama import Fore, Back, Style
+init(autoreset=True)
 def print_logo():
-    print('''
-╔═══╗ ╔════╗ ╔════╗ ╔═══╗ ╔╗   ╔═══╗     ╔══╗ ╔═══╗ ╔══╗ ╔═══╗ ╔══╗ ╔╗╔╗
-║╔══╝ ║ ╔╗ ║ ║ ╔╗ ║ ║╔══╝ ║║   ║╔══╝     ║╔═╝ ║╔══╝ ║╔╗║ ║╔═╗║ ║╔═╝ ║║║║
-║║╔═╗ ║ ║║ ║ ║ ║║ ║ ║║╔═╗ ║║   ║╚══╗     ║╚═╗ ║╚══╗ ║╚╝║ ║╚═╝║ ║║   ║╚╝║
-║║╚╗║ ║ ║║ ║ ║ ║║ ║ ║║╚╗║ ║║   ║╔══╝     ╚═╗║ ║╔══╝ ║╔╗║ ║╔╗╔╝ ║║   ║╔╗║
-║╚═╝║ ║ ╚╝ ║ ║ ╚╝ ║ ║╚═╝║ ║╚═╗ ║╚══╗     ╔═╝║ ║╚══╗ ║║║║ ║║║║  ║╚═╗ ║║║║
-╚═══╝ ╚════╝ ╚════╝ ╚═══╝ ╚══╝ ╚═══╝     ╚══╝ ╚═══╝ ╚╝╚╝ ╚╝╚╝  ╚══╝ ╚╝╚╝
-    ''')
+    print(Fore.LIGHTBLUE_EX + '╔═══╗', Fore.LIGHTRED_EX + '╔════╗', Fore.LIGHTYELLOW_EX + '╔════╗', Fore.LIGHTBLUE_EX + '╔═══╗', Fore.LIGHTGREEN_EX + '╔╗  ', Fore.LIGHTRED_EX + '╔═══╗', '   ', Fore.LIGHTBLUE_EX + '╔══╗', Fore.LIGHTRED_EX + '╔═══╗', Fore.LIGHTYELLOW_EX + '╔══╗', Fore.LIGHTBLUE_EX + '╔═══╗', Fore.LIGHTGREEN_EX + '╔══╗', Fore.LIGHTRED_EX + '╔╗╔╗')
+    print(Fore.LIGHTBLUE_EX + '║╔══╝', Fore.LIGHTRED_EX + '║ ╔╗ ║', Fore.LIGHTYELLOW_EX + '║ ╔╗ ║', Fore.LIGHTBLUE_EX + '║╔══╝', Fore.LIGHTGREEN_EX + '║║  ', Fore.LIGHTRED_EX + '║╔══╝', '   ', Fore.LIGHTBLUE_EX + '║╔═╝', Fore.LIGHTRED_EX + '║╔══╝', Fore.LIGHTYELLOW_EX + '║╔╗║', Fore.LIGHTBLUE_EX + '║╔═╗║', Fore.LIGHTGREEN_EX + '║╔═╝', Fore.LIGHTRED_EX + '║║║║')
+    print(Fore.LIGHTBLUE_EX + '║║╔═╗', Fore.LIGHTRED_EX + '║ ║║ ║', Fore.LIGHTYELLOW_EX + '║ ║║ ║', Fore.LIGHTBLUE_EX + '║║╔═╗', Fore.LIGHTGREEN_EX + '║║  ', Fore.LIGHTRED_EX + '║╚══╗', '   ', Fore.LIGHTBLUE_EX + '║╚═╗', Fore.LIGHTRED_EX + '║╚══╗', Fore.LIGHTYELLOW_EX + '║╚╝║', Fore.LIGHTBLUE_EX + '║╚═╝║', Fore.LIGHTGREEN_EX + '║║  ', Fore.LIGHTRED_EX + '║╚╝║')
+    print(Fore.LIGHTBLUE_EX + '║║╚╗║', Fore.LIGHTRED_EX + '║ ║║ ║', Fore.LIGHTYELLOW_EX + '║ ║║ ║', Fore.LIGHTBLUE_EX + '║║╚╗║', Fore.LIGHTGREEN_EX + '║║  ', Fore.LIGHTRED_EX + '║╔══╝', '   ', Fore.LIGHTBLUE_EX + '╚═╗║', Fore.LIGHTRED_EX + '║╔══╝', Fore.LIGHTYELLOW_EX + '║╔╗║', Fore.LIGHTBLUE_EX + '║╔╗╔╝', Fore.LIGHTGREEN_EX + '║║  ', Fore.LIGHTRED_EX + '║╔╗║')
+    print(Fore.LIGHTBLUE_EX + '║╚═╝║', Fore.LIGHTRED_EX + '║ ╚╝ ║', Fore.LIGHTYELLOW_EX + '║ ╚╝ ║', Fore.LIGHTBLUE_EX + '║╚═╝║', Fore.LIGHTGREEN_EX + '║╚═╗', Fore.LIGHTRED_EX + '║╚══╗', '   ', Fore.LIGHTBLUE_EX + '╔═╝║', Fore.LIGHTRED_EX + '║╚══╗', Fore.LIGHTYELLOW_EX + '║║║║', Fore.LIGHTBLUE_EX + '║║║║ ', Fore.LIGHTGREEN_EX + '║╚═╗', Fore.LIGHTRED_EX + '║║║║')
+    print(Fore.LIGHTBLUE_EX + '╚═══╝', Fore.LIGHTRED_EX + '╚════╝', Fore.LIGHTYELLOW_EX + '╚════╝', Fore.LIGHTBLUE_EX + '╚═══╝', Fore.LIGHTGREEN_EX + '╚══╝', Fore.LIGHTRED_EX + '╚═══╝', '   ', Fore.LIGHTBLUE_EX + '╚══╝', Fore.LIGHTRED_EX + '╚═══╝', Fore.LIGHTYELLOW_EX + '╚╝╚╝', Fore.LIGHTBLUE_EX + '╚╝╚╝ ', Fore.LIGHTGREEN_EX + '╚══╝', Fore.LIGHTRED_EX + '╚╝╚╝')
 
 #  Copyright (c) 2021 videxerion
