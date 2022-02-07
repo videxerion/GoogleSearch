5820ce99124ed1c9cfdbcac0ee7663ea93e61ddb
videxerion <videxytgamer@gmail.com>
2022-02-07 14:55:54 +0000

    added timer

--- gs_modules/google.py
+++ gs_modules/google.py
@@ -1,8 +1,12 @@
 import requests
 import time
+import os
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
+from colorama import init
+from colorama import Fore
 
+init(autoreset=True)
 
 class GoogleBan(Exception):
     def __init__(self):
@@ -26,18 +30,26 @@ class google_module():
             #time.sleep(0.5)
             html = self.session.get(f'https://www.google.com/search?q={request}&start={g}0', headers=self.headres)
             if html.status_code == 429:
-                raise GoogleBan
-            soup = BeautifulSoup(html.text, 'html.parser')
-            links_not_clean = soup.find_all('div', {'class': 'yuRUbf'})
-            links_clean = []
-            for i in range(len(links_not_clean)):
-                string = str(links_not_clean[i])
-                if string.find('href') != -1:
-                    links_clean.append(unquote(unquote(string[string.find('" href="') + 8: string.find('" ping="')])))
-            if links_clean == []:
-                return return_array
+                for i in range(600):
+                    os.system('clear')
+                    print(Fore.RED + '[-] ERROR')
+                    print(Fore.RED + '[-] GOOGLE BAN!')
+                    print(Fore.YELLOW + '[#] Wait 10 minutes...')
+                    print(Fore.YELLOW + f'[#] {i}/600 s')
+                    time.sleep(1)
+                g -= 1
             else:
-                for i in range(len(links_clean)):
-                    return_array.append(links_clean[i])
-            g += 1
+                soup = BeautifulSoup(html.text, 'html.parser')
+                links_not_clean = soup.find_all('div', {'class': 'yuRUbf'})
+                links_clean = []
+                for i in range(len(links_not_clean)):
+                    string = str(links_not_clean[i])
+                    if string.find('href') != -1:
+                        links_clean.append(unquote(unquote(string[string.find('" href="') + 8: string.find('" ping="')])))
+                if not links_clean:
+                    return return_array
+                else:
+                    for i in range(len(links_clean)):
+                        return_array.append(links_clean[i])
+                g += 1
 #  Copyright (c) 2021 videxerion
